479c5609ba9ecc9f1ce3952e8f600124dbbc9f06
isabelapreviato <isabelapreviato@hotmail.com>
2019-02-15 17:43:59 -0200

    segundo

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ const options=document.querySelector('.options');
 
 const aboutus=document.querySelector('.about-us');
 
+const jogo=document.querySelector('.areaGrid');
+
 function jogar () {
     play.style.display='none';
     options.style.display='none';
     aboutus.style.display='none';
+    jogo.style.display='flex';
+
     // fade='slow';
 }
 
 play.onclick=jogar;
+
+
+
+function caixa () {
+}
