7ed26896670f07c4436d3a4fa8f3d8c735c99cfb
isabelapreviato <isabelapreviato@hotmail.com>
2019-02-16 18:00:31 -0200

    terceiro

--- script.js
+++ script.js
@@ -8,18 +8,23 @@ const aboutus=document.querySelector('.about-us');
 
 const jogo=document.querySelector('.areaGrid');
 
+// grid
+
+const gridd=document.querySelector('.gridd');
+
 function jogar () {
     play.style.display='none';
+    // play.style.color='aquamarine';
     options.style.display='none';
     aboutus.style.display='none';
     jogo.style.display='flex';
-
-    // fade='slow';
+   // fade='slow';
 }
-
-play.onclick=jogar;
-
-
 
 function caixa () {
+    gridd.style.color='black';
 }
+
+gridd.onclick=caixa
+play.onclick=jogar;
+
